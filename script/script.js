65408eadcb50e971d4448c8c83f5cb0bb17b4eb9
Ashwini Gupta <ashwinigupta8052653693@gmail.com>
2022-09-23 16:02:35 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.getElementById('sign-up').addEventListener('click', signUpAlert);
+document.getElementById('log-in').addEventListener('click', logInAlert);
+
+function signUpAlert() {
+    alert('The signUp function is not implemented yet');
+}
+function logInAlert() {
+    alert('The logIn function is also not implemented yet');
+}
